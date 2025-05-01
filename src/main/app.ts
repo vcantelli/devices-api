a7e69dd3476ea4e428f38e8b3fe6f2dbdbd11e9e
Victor Cantelli <victor@evnts.com.br>
2025-05-01 05:42:44 -0300

    chore(security): add helmet middleware to protect HTTP headers

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import deviceRoutes from "@/presentation/routes/deviceRoutes";
 import { errorHandler } from "@/presentation/middlewares/errorHandler";
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
